a4fab0e5f739eab46330363612e0b441dccfcccd
andrejici <ivanandrejic@yahoo.com>
2017-11-15 22:01:04 +0100

    add lombok

--- src/main/java/com/demo/catalog/domain/User.java
+++ src/main/java/com/demo/catalog/domain/User.java
@@ -3,19 +3,19 @@ package com.demo.catalog.domain;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+import lombok.ToString;
+
 @Document(collection = "users")
+@Data
+@ToString(exclude = "password")
 public class User {
 
 	private @Id String id;
 
 	private String username;
+	
+	private String password;
 
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
 
 }
